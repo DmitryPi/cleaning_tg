d8d4a70b268f41c5f36c572187fc2b2ce663d12f
Dmitry G <sdprecovery@gmail.com>
2022-07-03 14:00:22 +0300

    users.py UserRole User build_user

---
+++ modules/users.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from dataclasses import dataclass
+from datetime import datetime
+
+
+class UserRole(Enum):
+    USER = 'Пользователь'
+    MANAGER = 'Менеджер'
+
+
+@dataclass
+class User:
+    uid: int
+    username: str
+    first_name: str
+    full_name: str
+    phone_num: int
+    role: UserRole
+    created: str
+    updated: str
+
+
+def build_user(user_data, tg_data: dict) -> User
+    """user_data - из json файла; tg_data - объект телеграм пользователя"""
+    now = str(datetime.now())
+    user = User(
+        tg_data['id'],
+        tg_data['username'],
+        tg_data['first_name'],
+        user_data['full_name'],
+        user_data['phone_num'],
+        UserRole.USER.value,
+        now,
+        now,
+    )
+    return user
