379b651e043af6b48866c14e08f9a1d1e6a93a15
Dmitry G <sdprecovery@gmail.com>
2022-07-03 14:13:10 +0300

    TestUsers refactoring

--- modules/tests/test_users.py
+++ modules/tests/test_users.py
@@ -1,30 +1,28 @@
 from unittest import TestCase
 
+from ..utils import load_json
 from ..users import UserRole, build_user
 
 
 class TestUsers(TestCase):
     def setUp(self):
-        pass
-
-    def test_build_user(self):
-        local_data = {
-            "full_name": "Иванов Иван Иванович",
-            "phone_num": 79787873919
-        }
-        tg_data = {
+        self.users = load_json('assets/users.json')
+        self.user_tg = {
             'is_bot': False,
             'username': 'DmitrydevPy',
             'first_name': 'Dmitry',
             'id': 5156307333,
             'language_code': 'ru'
         }
-        user = build_user(local_data, tg_data)
-        assert user.uid == tg_data['id']
-        assert user.username == tg_data['username']
-        assert user.first_name == tg_data['first_name']
-        assert user.full_name == local_data['full_name']
-        assert user.phone_num == local_data['phone_num']
+
+    def test_build_user(self):
+        local_user = self.users[0]
+        user = build_user(self.users[0], self.user_tg)
+        assert user.uid == self.user_tg['id']
+        assert user.username == self.user_tg['username']
+        assert user.first_name == self.user_tg['first_name']
+        assert user.full_name == local_user['full_name']
+        assert user.phone_num == local_user['phone_num']
         assert user.role == UserRole.USER.value
         assert isinstance(user.created, str)
         assert isinstance(user.updated, str)
