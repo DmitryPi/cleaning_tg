46f7677ae6f519411dc321ceb3afaf52ef1dff97
Dmitry G <sdprecovery@gmail.com>
2022-07-03 13:01:28 +0300

    TelegramBot init command_start command_help command_role

--- modules/bot.py
+++ modules/bot.py
@@ -1,6 +1,21 @@
 from .db import Database
 from .utils import load_config
 
+from telegram import (
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    Update,
+)
+from telegram.ext import (
+    Application,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
 
 class TelegramBot:
     def __init__(self, api_token: str, config=None):
@@ -9,5 +24,23 @@ class TelegramBot:
         self.db = Database()
         self.db_conn = self.db.create_connection()
 
-    def run(self):
+    def command_start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         pass
+
+    def command_help(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        '''Помошник для вывода команд'''
+        pass
+
+    def command_role(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        '''Изменение роли пользователя'''
+        pass
+
+    def run(self):
+        '''Запустить бота'''
+        print(f'- {__class__.__name__} started')
+        # Create the Application and pass it your bot's token.
+        application = Application.builder().token(self.api_token).build()
+        # on different commands - answer in Telegram
+        application.add_handler(CommandHandler('start', self.command_start))
+        application.add_handler(CommandHandler('help', self.command_help))
+        application.add_handler(CommandHandler('role', self.command_role))
