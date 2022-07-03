f685b7d4aab13c5878cbc6c994c3ad4d183688ae
Dmitry G <sdprecovery@gmail.com>
2022-07-03 14:10:27 +0300

    TestUsers test_build_user

--- modules/tests/test_users.py
+++ modules/tests/test_users.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 
-from ..users import build_user
+from ..users import UserRole, build_user
 
 
-class TestUtils(TestCase):
+class TestUsers(TestCase):
     def setUp(self):
         pass
 
@@ -11,4 +11,20 @@ class TestUtils(TestCase):
         local_data = {
             "full_name": "Иванов Иван Иванович",
             "phone_num": 79787873919
-        }}
+        }
+        tg_data = {
+            'is_bot': False,
+            'username': 'DmitrydevPy',
+            'first_name': 'Dmitry',
+            'id': 5156307333,
+            'language_code': 'ru'
+        }
+        user = build_user(local_data, tg_data)
+        assert user.uid == tg_data['id']
+        assert user.username == tg_data['username']
+        assert user.first_name == tg_data['first_name']
+        assert user.full_name == local_data['full_name']
+        assert user.phone_num == local_data['phone_num']
+        assert user.role == UserRole.USER.value
+        assert isinstance(user.created, str)
+        assert isinstance(user.updated, str)
