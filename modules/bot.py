485d3af69786550371230363cb2ae7e53a9fa284
Dmitry G <sdprecovery@gmail.com>
2022-07-05 12:24:34 +0300

    TelegramBot error_handler

--- modules/bot.py
+++ modules/bot.py
@@ -1,10 +1,15 @@
 import re
+import html
+import json
+import logging
+import traceback
 
 from .db import Database
 from .users import UserRole, build_user
 from .utils import load_config, load_json
 
 from telegram import (
+    Bot,
     InlineKeyboardButton,
     InlineKeyboardMarkup,
     ReplyKeyboardMarkup,
@@ -131,7 +136,7 @@ class TelegramBot:
     async def command_review(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         """Создать клавиатуру для оценки 1-10"""
         try:
-            self.db.get_user(self.db_conn, update.effective_user.id)
+            user = self.db.get_user(self.db_conn, update.effective_user.id)
             msg = 'Оцените качество услуги:'
             score_btns = [
                 [InlineKeyboardButton(i, callback_data=i) for i in range(1, 6)],
@@ -139,6 +144,7 @@ class TelegramBot:
             ]
             reply_keyboard = [score_btns[0], score_btns[1]]
             reply_markup = InlineKeyboardMarkup(reply_keyboard)
+            context.user_data.update({'user': user})  # save current user
             await update.message.reply_text(msg, reply_markup=reply_markup)
             return 1
         except IndexError:
@@ -156,14 +162,9 @@ class TelegramBot:
 
     async def review_comment(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         """Принятие комментария пользователя и завершения диалога"""
-        score = context.user_data["review_score"]
+        score = context.user_data['review_score']
         comment = update.message.text.replace('/', '')
-        msg = [
-            'Спасибо за оставленный отзыв',
-            f'Ваша оценка: {score}',
-            f'Ваш комментарий: {comment}',
-        ]
-        msg = '\n'.join(msg)
+        msg = 'Спасибо за оставленный отзыв'
         await update.message.reply_text(msg)
         return ConversationHandler.END
 
@@ -222,6 +223,33 @@ class TelegramBot:
         await update.message.reply_text(msg, reply_markup=ReplyKeyboardRemove())
         return ConversationHandler.END
 
+    async def error_handler(self, update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Log the error and send a telegram message to notify the developer."""
+        # Log the error before we do anything else, so we can see it even if something breaks.
+        logging.error(msg="\n\nException while handling an update:", exc_info=context.error)
+
+        # traceback.format_exception returns the usual python message about an exception, but as a
+        # list of strings rather than a single string, so we have to join them together.
+        tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+        tb_string = "".join(tb_list)
+
+        # Build the message with some markup and additional information about what happened.
+        # You might need to add some logic to deal with messages longer than the 4096 character limit.
+        update_str = update.to_dict() if isinstance(update, Update) else str(update)
+        message = (
+            f"An exception was raised while handling an update\n"
+            f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+            "</pre>\n\n"
+            f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+            f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+            f"<pre>{html.escape(tb_string)}</pre>"
+        )
+
+        # Finally, send the message
+        await context.bot.send_message(
+            chat_id=self.config['TELEGRAM']['admin_id'], text=message, parse_mode=ParseMode.HTML
+        )
+
     def run(self):
         """Запустить бота"""
         print(f'- {__class__.__name__} started')
@@ -267,5 +295,7 @@ class TelegramBot:
         application.add_handler(role_conv_handler)
         application.add_handler(upload_conv_handler)
         application.add_handler(review_conv_handler)
+        # ...and the error handler
+        application.add_error_handler(self.error_handler)
         # Run the bot until the user presses Ctrl-C
         application.run_polling()
